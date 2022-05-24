94f44aa665db0810f88793f959ba5e1272ec8138
Jeremy <jeremylm21@gmail.com>
2022-05-23 20:29:16 -0600

    Adding election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -5,8 +5,11 @@
 # 4. The total number of votes each candidate won
 # 5. The winnder of the election based on popular vote
 
+# Add dependencies
+
 import csv
 import os
+from unicodedata import name
 
 cwd = os.getcwd()
 
@@ -24,6 +27,22 @@ file_to_load = os.path.join(r"C:\Users\Jeremy\Desktop\SKOOL\DA_Bootcamp\Python M
 
 file_to_save = os.path.join(r"C:\Users\Jeremy\Desktop\SKOOL\DA_Bootcamp\Python Module\Election-analysis", "analysis", "election_analysis.txt")
 
+# 1. Initialize a total vote counter
+
+total_votes = 0
+
+# Candidate options
+candidate_options = []
+
+# Declare the empty dictionary
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -38,6 +57,92 @@ with open(file_to_load) as election_data:
 
     headers = next(file_reader)
     print(headers)
+
+
+# Print each row in the CSV file. 
+
+    for row in file_reader:
+        
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+
+            # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+
+            # Begin tracking the candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+# Save the results to our text file
+with open (file_to_save, "w") as txt_file:
+
+    # Print out the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-----------------------\n"
+        f"Total Vots {total_votes:,}\n"
+        f"-----------------------\n")
+    print(election_results, end="")
+
+    #After printing the final vote count, save the final vote count to the text file. 
+    txt_file.write(election_results)
+
+    #Determine the percentage of votes for each candidate by looping through the counts.
+
+    # Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+
+        # Retrieve vote count of candidate.
+        votes = candidate_votes[candidate_name]
+
+        # Calculate the percentage of votes.
+        vote_percentage = float(votes)/float(total_votes) * 100
+
+        candidate_results = (
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        
+        #Print each candidate, their voter count, and percentage to the terminal\
+        print(candidate_results)
+
+        # Save the candidate results to the text file.
+        txt_file.write(candidate_results)
+
+        # Print each candidate, their vote count and percentage to terminal.
+
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Determine winning vote count and candidate
+
+        # Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+            #If true then set winning_count = votes and winning_percent = vote_percent
+            winning_count = votes
+            winning_percentage  = vote_percentage
+
+            # Set the winning+candidate equal to the candidate's name
+            winning_candidate = candidate_name
+    
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
+
+    #print(winning_candidate_summary)
+
 
 
 
